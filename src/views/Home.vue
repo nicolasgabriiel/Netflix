d01f923c8dc3c0dfbbdc141cd620a624bd62ef4d
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-09 21:58:05 -0300

    Upando arquivos iniciais usando Vue, TypeScript, Tailwind e vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>ola eu sou a home</h1>
+  </main>
+</template>
+
+<script  lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name:'Home'
+})
+</script>
