11b3112796d081331cb76c6b318dd9ef4fa01661
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-13 21:47:30 -0300

    Estilizando a maior parte dos compoentens do formulário

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -1,25 +1,32 @@
 <template>
-    <section class="formulario">
-        <h1>Entrar</h1>
-        <form>
-            <input type="text">
-            <input type="text">
-            <button>Entrar</button>
-        </form>
-        <div>
+    <div class="conter w-full mt-8">
+        <section class="bg-black/75 p-5px w-128 h-40 box-border text-white">
+            <h1 class="text-4xl text-white mb-5">Entrar</h1>
+            <form>
+                <input type="text" placeholder="Email ou número de telefone " 
+                class="block bg-[#333333] w-full p-4 mb-5 rounded-md">
+                <input type="text" placeholder="Senha"
+                class="block bg-[#333333] w-full p-4 rounded-md mb-7">
+                <button class="block bg-[#E50914] w-full p-4 mb-5 rounded-md"
+                >Entrar</button>
+            </form>
             <div>
+                <div>
+                    <div>
 
+                    </div>
+                    <p>Esqueceu a senha?</p>
+                </div>
+                <a>Precisa de ajuda?</a>
             </div>
-            <a>Precisa de ajuda?</a>
-        </div>
-        <h2>Novo Por aqui? <a>Assine agora</a>.</h2>
-        <div>
-            <p>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô. <button type="button">Saiba mais.</button></p>
-            <p class="hide">As informações recolhidas pelo Google reCAPTCHA estão sujeitas à <a>Política de Privacidade</a> e <a>Termos de Uso</a>, e são usadas para oferecer, manter e melhorar o serviço reCAPTCHA e por questões de segurança (não são usadas para exibir anúncios personalizados pelo Google).</p>
-        </div>
-        
-    </section>
-    
+            <h2>Novo Por aqui? <a>Assine agora</a>.</h2>
+            <div>
+                <p>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô. <button type="button">Saiba mais.</button></p>
+                <p class="hide">As informações recolhidas pelo Google reCAPTCHA estão sujeitas à <a>Política de Privacidade</a> e <a>Termos de Uso</a>, e são usadas para oferecer, manter e melhorar o serviço reCAPTCHA e por questões de segurança (não são usadas para exibir anúncios personalizados pelo Google).</p>
+            </div>
+            
+        </section>
+    </div>
 </template>
 
 <script lang="ts">
@@ -33,6 +40,14 @@ export default  defineComponent({
 
 <style scoped>
 .hide{
-    display: none;
+    opacity: 0;
+}
+.conter{
+    display: flex;
+    justify-content: center;
+}
+.formulario{
+    background: white;
+    padding: 60px 68px 40px 68px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,20 @@ module.exports = {
       height: {
       '128': '1049px',
       '44': '167px',
+      '40': '660px',
+    },
+    width: {
+      '128': '450px',
     },
       backgroundImage: {
       'hero-pattern': "url('./src/assets/bg-tela-inicial.jpg')",
-    }
+    },
+    padding:{
+      '5px': '60px 68px 40px 68px'
+    },
+    colors: {
+      'regal-blue': '#33333',
+    },
     }
   },
   plugins: [],
