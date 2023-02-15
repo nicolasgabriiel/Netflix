5008a97ac5f9bbdfb55750a7607ba8b0e6086f21
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-14 22:28:03 -0300

    Estruturando página de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an login page</h1>
-  </div>
+  <section>
+    <h1>Quem está assistindo?</h1>
+    <section>
+        <div>
+          <div class="imagem"></div>
+          <p>Meu Perfil</p>
+        </div>
+        <div>
+          <div class="imagemmais"></div>
+          <p>Adicionar Perfil</p>
+        </div>
+    </section>
+    <button>Gerenciar Perfil</button>
+  </section>
 </template>
 
 <style>
