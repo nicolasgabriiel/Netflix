2c342d7bdceb9aa9e68af8e48091d318092a41f4
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-15 22:21:52 -0300

    Começando a estilização da tela de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,9 @@
 <template>
-  <section>
-    <h1>Quem está assistindo?</h1>
-    <section>
+  <header>sadas</header>
+  <main class="w-full h-screen bg-[#141414] flex justify-center">
+    <div class="mt-36">
+      <h1 class="text-7xl text-white bold">Quem está assistindo?</h1>
+      <div>
         <div>
           <div class="imagem"></div>
           <p>Meu Perfil</p>
@@ -10,10 +12,18 @@
           <div class="imagemmais"></div>
           <p>Adicionar Perfil</p>
         </div>
-    </section>
-    <button>Gerenciar Perfil</button>
-  </section>
+      </div>
+      <button class="border-[#808080] border-2 pt-3 pb-3 pl-10 pr-10 mb-5 text-2xl text-[#808080] hover:text-white hover:border-white" 
+      >Gerenciar Perfis</button>
+
+    </div>
+  </main>
 </template>
 
-<style>
+<style scoped>
+header {
+  width: 100%;
+  height: 68px;
+  background-image: linear-gradient(to bottom, #060606, #141414);
+}
 </style>
