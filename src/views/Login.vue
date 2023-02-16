5f30c627e0e3b44dea203c5366554bf76abb40ae
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-16 20:19:31 -0300

    Desenvolvendo a lógica do hover

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,12 +4,12 @@
     <div class="mt-36 ">
       <h1 class="text-7xl text-white bold mb-14">Quem está assistindo?</h1>
       <div class="flex justify-center">
-        <div class="mr-10 hover:cursor-pointer " >
+        <div class="mousefi mr-10 hover:cursor-pointer mouse" >
           <div class="perfil w-48 h-48"></div>
           <p class="text-2xl text-[#808080] text-center mt-2">Meu Perfil</p>
         </div>
-        <div>
-          <div class="adicionar w-48 h-48 hover:cursor-pointer"></div>
+        <div class="mouseadicionar">
+          <div class="adicionar w-48 h-48 hover:cursor-pointer hover:bg-white"></div>
           <p class="text-2xl text-[#808080] text-center mt-2">Adicionar Perfil</p>
         </div>
       </div>
@@ -30,6 +30,12 @@ header {
 .perfil{
   background-image: url(../assets/icons/velhote.png);
   background-size: contain;
+}
+.mousefi:hover  p, .mouseadicionar:hover p{
+  color: white;
+}
+.mousefi:hover  .perfil, .mouseadicionar:hover .adicionar{
+  background-color: white;
 }
 .adicionar{
   background-image: url(../assets/adicionar.png);
