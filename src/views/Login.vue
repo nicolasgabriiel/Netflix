6d90b13d18b0dc9a2d3b1db438643415eb4a89e3
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-15 22:50:19 -0300

    adicionando icones ao projeto

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <header>sadas</header>
   <main class="w-full h-screen bg-[#141414] flex justify-center">
-    <div class="mt-36">
-      <h1 class="text-7xl text-white bold">Quem está assistindo?</h1>
+    <div class="mt-36 ">
+      <h1 class="text-7xl text-white bold mb-14">Quem está assistindo?</h1>
       <div>
         <div>
           <div class="imagem"></div>
-          <p>Meu Perfil</p>
+          <p class="text-2xl text-[#808080]">Meu Perfil</p>
         </div>
         <div>
           <div class="imagemmais"></div>
-          <p>Adicionar Perfil</p>
+          <p class="text-2xl text-[#808080]">Adicionar Perfil</p>
         </div>
       </div>
       <button class="border-[#808080] border-2 pt-3 pb-3 pl-10 pr-10 mb-5 text-2xl text-[#808080] hover:text-white hover:border-white" 
