d4be1ff2f02b7f9a574acecbcccf99dc9e7fd53d
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-15 23:04:43 -0300

    quase terminadno a estilização da página de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,19 +3,20 @@
   <main class="w-full h-screen bg-[#141414] flex justify-center">
     <div class="mt-36 ">
       <h1 class="text-7xl text-white bold mb-14">Quem está assistindo?</h1>
-      <div>
-        <div>
-          <div class="imagem"></div>
-          <p class="text-2xl text-[#808080]">Meu Perfil</p>
+      <div class="flex justify-center">
+        <div class="mr-10 hover:cursor-pointer " >
+          <div class="perfil w-48 h-48"></div>
+          <p class="text-2xl text-[#808080] text-center mt-2">Meu Perfil</p>
         </div>
         <div>
-          <div class="imagemmais"></div>
-          <p class="text-2xl text-[#808080]">Adicionar Perfil</p>
+          <div class="adicionar w-48 h-48 hover:cursor-pointer"></div>
+          <p class="text-2xl text-[#808080] text-center mt-2">Adicionar Perfil</p>
         </div>
       </div>
-      <button class="border-[#808080] border-2 pt-3 pb-3 pl-10 pr-10 mb-5 text-2xl text-[#808080] hover:text-white hover:border-white" 
+      <div class="flex justify-center mt-20">
+        <button class="border-[#808080] border-2 pt-3 pb-3 pl-10 pr-10 mb-5 text-2xl text-[#808080] hover:text-white hover:border-white" 
       >Gerenciar Perfis</button>
-
+      </div>
     </div>
   </main>
 </template>
@@ -25,5 +26,15 @@ header {
   width: 100%;
   height: 68px;
   background-image: linear-gradient(to bottom, #060606, #141414);
+}
+.perfil{
+  background-image: url(../assets/icons/velhote.png);
+  background-size: contain;
+}
+.adicionar{
+  background-image: url(../assets/adicionar.png);
+  background-size:65%;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
