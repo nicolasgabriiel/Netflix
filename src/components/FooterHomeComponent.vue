bf281ffb89f730fdab4c9cd7dc8a87b279d525a8
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-18 19:37:19 -0300

    estruturando o footer da tela de login

--- src/components/FooterHomeComponent.vue
+++ src/components/FooterHomeComponent.vue
@@ -1,6 +1,21 @@
 <template>
     <footer class="text-white text-3xl">
-        <h1>AQUI ESTA O FOOTER</h1>
+        <h1>Dúvidas? Ligue 0800 591 8942</h1>
+        <div>
+            </div>
+                <a>Perguntas frequentes</a>
+                <a>Central de Ajuda</a>
+                <a>Termos de Uso</a>
+                <a>Privacidade</a>
+            <div>
+            <div>
+                <a>Preferências de cookies</a>
+                <a>Informações corporativas</a> 
+            </div>
+            <div>
+                <input type="checkbox">
+            </div>
+        </div>
 
     </footer>
 </template>
