e4fb429f85da82ea782adf827b612dc2af8e58a1
Nicolas <nicolasgabriel.developer@gmail.com>
2023-05-23 19:58:14 -0300

    Transforamndo o Tailwind em CSS puro

--- src/components/FormularioComponent.vue
+++ src/components/FormularioComponent.vue
@@ -1,67 +1,142 @@
 <template>
-    <div class="conter w-full mt-8">
-        <section class="bg-black/75 p-5px w-128 h-40 box-border text-white">
-            <h1 class="text-4xl text-white mb-5">Entrar</h1>
+    <div class="box-conter">
+        <section class="container-formulario">
+            <h1 class="text-4xl text-white mb-5 texto-principal">Entrar</h1>
             <form>
-                <input type="text" placeholder="Email ou número de telefone " 
-                class="block bg-[#333333] w-full p-4 mb-5 rounded-md">
-                <input type="text" placeholder="Senha"
-                class="block bg-[#333333] w-full p-4 rounded-md mb-10">
-                <router-link to="/Login">      
-                    <button class="block bg-[#E50914] w-full p-3 mb-5 rounded-md text-base font-bold"
-                    >Entrar</button>
-                </router-link>  
+                <input type="text" placeholder="Email ou número de telefone " class="inputs">
+                <input type="text" placeholder="Senha" class="inputs input2">
+                <router-link to="/Login">
+                    <button class="botao-principal">Entrar</button>
+                </router-link>
             </form>
-            <div class="flex justify-between">
-                <div class="flex mb-10">
+            <div class="container-inferior">
+                <div class="box-inferior">
                     <input type="checkbox">
                     <p>Lembre-se de mim</p>
                 </div>
                 <a>Precisa de ajuda?</a>
             </div>
-            <h2 class="mb-3">Novo Por aqui? <a>Assine agora</a>.</h2>
+            <h2 class="texto-inferior">Novo Por aqui? <a>Assine agora</a>.</h2>
             <div>
-                <p class="text-sm mb-3">Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô. 
-                    <button type="button" @click="mostrarMais" v-bind:class="{hide: !saibaMais}" class="text-[#15009C]">Saiba mais.</button>
+                <p class="textos">Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô.
+                    <button type="button" @click="mostrarMais" v-bind:class="{ hide: !saibaMais }" class="text-button">Saiba
+                        mais.</button>
                 </p>
-                <p  v-bind:class="{hide: saibaMais}" class="text-sm">As informações recolhidas pelo Google reCAPTCHA estão sujeitas à <a class="decoration-solid text-[#15009C]" href="https://policies.google.com/privacy" target="_blank">Política de Privacidade</a> e <a class="decoration-solid text-[#15009C]" target="_blank" href="https://policies.google.com/terms
-                ">Termos de Uso</a>, e são usadas para oferecer, manter e melhorar o serviço reCAPTCHA e por questões de segurança (não são usadas para exibir anúncios personalizados pelo Google).</p>
+                <p v-bind:class="{ hide: saibaMais }" class="textos">As informações recolhidas pelo Google reCAPTCHA estão
+                    sujeitas à <a class="decoration text-button" href="https://policies.google.com/privacy"
+                        target="_blank">Política de Privacidade</a> e <a class="decoration text-button" target="_blank"
+                        href="https://policies.google.com/terms
+                ">Termos de Uso</a>, e são usadas para oferecer, manter e melhorar o serviço reCAPTCHA e por questões
+                    de segurança (não são usadas para exibir anúncios personalizados pelo Google).</p>
             </div>
-            
+
         </section>
     </div>
 </template>
 
+
+<style scoped>
+.hide {
+    opacity: 0;
+}
+.box-conter {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+    width: 100%;
+}
+.container-formulario {
+    background-color: rgb(0 0 0 / 0.75);
+    padding: 60px 68px 40px 68px;
+    width: 450px;
+    height: 660px;
+    box-sizing: border-box;
+    color: white;
+}
+.texto-principal {
+    font-size: 2.25rem
+        /* 36px */
+    ;
+    line-height: 2.5rem
+        /* 40px */
+    ;
+    margin-bottom: 1.25rem
+        /* 20px */
+    ;
+    color: white;
+}
+.inputs {
+    display: block;
+    background-color: #333333;
+    width: 100%;
+    padding: 1rem;
+    border-radius: 0.375rem;
+    margin-bottom: 1.25rem;
+}
+.input2 {
+    margin-bottom: 2.5rem;
+}
+.botao-principal {
+    display: block;
+    background-color: #e50914;
+    width: 100%;
+    padding: 0.75rem;
+    margin: 0 0 1.25rem 0;
+    border-radius: 0.375rem;
+    font-size: 1rem
+        /* 16px */
+    ;
+    line-height: 1.5rem
+        /* 24px */
+    ;
+    font-weight: 700px;
+}
+.container-inferior {
+    display: flex;
+    justify-content: space-between;
+}
+.box-inferior {
+    margin-bottom: 2.5rem
+        /* 40px */
+    ;
+    display: flex;
+}
+.texto-inferior {
+    margin-bottom: 0.75rem
+        /* 12px */
+}
+.textos {
+    font-size: 0.875rem
+        /* 14px */
+    ;
+    line-height: 1.25rem
+        /* 20px */
+    ;
+    margin-bottom: 0.75rem;
+}
+.text-button {
+    color: #15009c;
+}
+.decoration {
+    text-decoration: solid;
+}</style>
 <script lang="ts">
 import { defineComponent } from 'vue'
-export default  defineComponent({
+export default defineComponent({
     name: 'FormularioComponent',
-    methods:{
-        mostrarMais(){
-            this.saibaMais = false   
+    methods: {
+        mostrarMais() {
+            this.saibaMais = false
         }
     },
-    data(){
-        return{
+    data() {
+        return {
             saibaMais: true,
 
         }
     }
 })
-   
+
 
 </script>
 
-<style scoped>
-.hide{
-    opacity: 0;
-}
-.conter{
-    display: flex;
-    justify-content: center;
-}
-.formulario{
-    background: white;
-    padding: 60px 68px 40px 68px;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
   <!--          BACKGROUND       -->
-  <section class="w-full h-128 bg-black absolute z-0" >
-    <img src="../assets/bg-tela-inicial.jpg" class="absolute opacity-40">
+  <section class="principal-section" >
+    <img src="../assets/bg-tela-inicial.jpg" class="imagem-fundo">
   </section>
-  <router-link to="/"><img src="../assets/logo.png" alt="" class="w-44 pt-5 ml-12"></router-link>
+  <router-link to="/"><img src="../assets/logo.png" alt="" class="logo"></router-link>
   <!-- Componentes   -->
   <FormularioComponent/>
   <FooterComponent/>
   
   
 </template>
+
+<style scoped>
+.principal-section{
+width: 100%;
+height: 100vh;
+background-color: black;
+position: absolute;
+z-index: -1;
+}
+.imagem-fundo{
+  opacity: 0.4;
+  position: absolute;
+  z-index: -2;
+  }
+  .logo{
+    width: 11rem;
+    padding-top: 1.25rem;
+    margin-left: 3rem;
+  }
+</style>
 
 <script  lang="ts">
 import { defineComponent } from 'vue';
@@ -26,4 +46,3 @@ export default defineComponent({
 })
 </script>
 
-s
